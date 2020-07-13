14eec64ec342db1611bc2d5970a67263b1b8da05
Richard Cyrus <richard.cyrus@rcyrus.com>
2020-07-13 13:18:36 -0400

    Update Packages

--- controllers/burger.js
+++ controllers/burger.js
@@ -5,20 +5,21 @@
  * (c) 2019 Richard Cyrus <richard.cyrus@rcyrus.com>
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../models');
+const db = require("../models");
 
 // Display the default page for the application.
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
     db.burger.findAll()
-        .then((data) => {
-            res.render('index', { burgers: data });
+        .then((results) => {
+            data = results.map((i) => i.get({ plain: true }));
+            res.render("index", { burgers: data });
         }).catch((err) => next(err));
 });
 
 // Route handler to create a new record based on user submission.
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
     const burgerName = req.body.burgerName;
 
     db.burger.create({ burgerName: burgerName })
@@ -29,7 +30,7 @@ router.post('/', (req, res, next) => {
 
 // Route handler to update a record when the `Devour It!` button is
 // clicked.
-router.put('/', (req, res, next) => {
+router.put("/", (req, res, next) => {
     const id = req.body.id;
 
     db.burger.update({ devoured: true }, { where: { id: id } })
